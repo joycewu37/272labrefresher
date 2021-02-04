806921a81e7081462ef6bee5d536aba9ecb8b712
Joyce Wu <thejoycewu@gmail.com>
2021-02-04 15:50:57 -0500

    delete task fixtures and add alphabetical task scope by name

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
-    has_many :chores
-    has_many :children, through: :chores
+  has_many :chores
+  has_many :children, through: :chores
 
-    validates_presence_of :name
-    validates_numericality_of :points, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_presence_of :name
+  validates_numericality_of :points, :only_integer => true, :greater_than_or_equal_to => 0
 
-
+  scope :alphabetical, -> { order('name') }
 end
