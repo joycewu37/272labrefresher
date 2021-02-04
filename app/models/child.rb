4d3caa481dc5cd364b6bd66706fc2f6f33dbe794
Joyce Wu <thejoycewu@gmail.com>
2021-02-04 15:28:46 -0500

    deleted children fixtures to pass last test case that should pass up to now

--- app/models/child.rb
+++ app/models/child.rb
@@ -6,6 +6,7 @@ class Child < ApplicationRecord
   validates_presence_of :last_name
 
   scope :alphabetical, -> { order('last_name, first_name') }
+  scope :active, -> { where(active: true) }
 
   def name
     return first_name + " " + last_name
