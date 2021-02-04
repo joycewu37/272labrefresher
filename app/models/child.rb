4fa1537e60c73a080ee5494594d8432796a0fcf3
Joyce Wu <thejoycewu@gmail.com>
2021-02-04 15:24:35 -0500

    added alphabetical scope to child

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,6 +5,8 @@ class Child < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
 
+  scope :alphabetical, -> { order('last_name, first_name') }
+
   def name
     return first_name + " " + last_name
   end    
