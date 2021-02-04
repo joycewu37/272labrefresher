f274a45e4d2821adaf19ea89bd5fa59a608dd8c6
Joyce Wu <thejoycewu@gmail.com>
2021-02-04 15:23:24 -0500

    added name method to child

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,5 +3,9 @@ class Child < ApplicationRecord
   has_many :tasks, through: :chores
 
   validates_presence_of :first_name
-  validates_presence_of :last_name    
+  validates_presence_of :last_name
+
+  def name
+    return first_name + " " + last_name
+  end    
 end
