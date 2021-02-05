73b3c118f572fb7480e81cf9cb9badbf2d161d24
Joyce Wu <thejoycewu@gmail.com>
2021-02-05 11:07:25 -0500

    done scope and upcoming scope for chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -7,4 +7,6 @@ class Chore < ApplicationRecord
   scope :by_task, -> { joins(:task).order('name') }
   scope :chronological, -> { joins(:task).order('due_on, completed') }
   scope :pending, -> { where(completed: false) }
+  scope :done, -> { where(completed: true) }
+  scope :upcoming, -> { where('due_on >= ?', Time.now.to_date) }
 end
