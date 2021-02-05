74560597337650430c672c1b90682e91d6a385c5
Joyce Wu <thejoycewu@gmail.com>
2021-02-05 11:09:48 -0500

    added status method for chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -9,4 +9,10 @@ class Chore < ApplicationRecord
   scope :pending, -> { where(completed: false) }
   scope :done, -> { where(completed: true) }
   scope :upcoming, -> { where('due_on >= ?', Time.now.to_date) }
+  scope :past, -> { where('due_on < ?', Time.now.to_date) }
+
+  def status
+    return "Completed" if completed
+    return "Pending"
+  end
 end
